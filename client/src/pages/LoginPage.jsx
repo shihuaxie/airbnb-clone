aec4bacf93863e467d52126ed1c6077faec7a1e0
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-05-22 16:44:48 +1000

    feat: create signin and login pages

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,20 @@
+import {Link} from "react-router-dom";
 export default function LoginPage() {
     return(
-        <div>
-            <form>
-
-            </form>
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-64">
+                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <form className="max-w-md mx-auto">
+                    <input type="text" placeholder="your@email.com"/>
+                    <input type="password" placeholder="password"/>
+                    <button className="primary">Login</button>
+                    {/*link to register*/}
+                    <div className="text-center py-2 text-gray-500">
+                        Don't have an account yet?
+                        <Link className="underline text-black" to={"/register"}>Register Now</Link>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
