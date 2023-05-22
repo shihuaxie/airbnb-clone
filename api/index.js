f6f387a76b39f8eefa86b98f2eb05f10ec880e84
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-05-22 22:53:12 +1000

    commit

--- api/index.js
+++ api/index.js
@@ -1,26 +1,38 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const User = require('./models/User');
 require('dotenv').config()
 const app = express();
 
+const bcryptSalt = bcrypt.genSaltSync(10)
+
 app.use(express.json());
 app.use(cors({
-    credentials:true,
-    origin:'http://127.0.0.1:5173',
+    credentials: true,
+    origin: 'http://127.0.0.1:5173',
 }))
 
 // connect to my mangodb
 mongoose.connect(process.env.MONGO_URL);
 
-app.get('/test', (req,res) => {
+app.get('/test', (req, res) => {
     res.json('test ok')
 });
 
-app.post('/register',(req, res) => {
-   const {name, email, password} = req.body;
-    res.json({name, email, password})
+app.post('/register', async (req, res) => {
+    const {name, email, password} = req.body;
+
+    const userDoc = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password, bcryptSalt),
+    });
+
+    res.json(userDoc);
 })
 
 app.listen(4000);
+
 // uKHsbF5EeiSpYu6o
