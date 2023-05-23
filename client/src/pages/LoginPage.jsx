6b803b8577f3416243f63944e5e8e180674cde7a
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-05-23 13:45:41 +1000

    feat: loginpage db

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,12 +1,34 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
+import axios from 'axios';
+
 export default function LoginPage() {
-    return(
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function handleLoginSubmit(e) {
+        e.preventDefault();
+        try {
+            await axios.post('/login', {email, password});
+            alert('Login Successful')
+        } catch (err) {
+            alert('Login failed')
+        }
+    }
+
+    return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Login</h1>
-                <form className="max-w-md mx-auto">
-                    <input type="text" placeholder="your@email.com"/>
-                    <input type="password" placeholder="password"/>
+                <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
+                    <input type="text"
+                           placeholder="your@email.com"
+                           value={email}
+                           onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" placeholder="password"
+                           value={password}
+                           onChange={(e) => setPassword(e.target.value)}/>
                     <button className="primary">Login</button>
                     {/*link to register*/}
                     <div className="text-center py-2 text-gray-500">
