d92010d076a8c6c86af3778e8385db947faf5b37
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-05-25 13:32:17 +1000

    upload photo by link back-end

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
 const User = require('./models/User');
 require('dotenv').config()
 const app = express();
@@ -14,6 +15,7 @@ const jwtSecret = 'ak7hffieu3ybcxbajk5vf';
 //middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
     credentials: true,
     origin: 'http://127.0.0.1:5173',
@@ -89,6 +91,18 @@ app.get('/profile', (req, res) => {
 app.post('/logout', (req, res) => {
     res.cookie('token', '').json(true);
 })
+
+//upload photo
+app.post('/upload-by-link', async (req, res) => {
+    const {link} = req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname + '/uploads/' + newName,
+    });
+    res.json(newName);
+})
+
 
 app.listen(4000);
 
