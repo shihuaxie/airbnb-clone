df1e288c923ad99abea60dfdfaa53caefc442a40
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-05-25 13:32:40 +1000

    upload photo by link front-end

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,9 +1,52 @@
 import {Link, useParams} from "react-router-dom";
-import {act} from "react-dom/test-utils";
+import {useState} from "react";
+import Perks from "../Perks.jsx";
+import axios from "axios";
 
 export default function PlacesPage() {
     const {action} = useParams();
     //console.log(action); output---new
+    const [title, setTitle] = useState('');
+    const [address, setAddress] = useState('');
+    const [addedPhotos, setAddedPhotos] = useState([]);
+    const [photoLink, setPhotoLink] = useState('');
+    const [description, setDescription] = useState('');
+    const [perks, setPerks] = useState([]);
+    const [extraInfo, setExtraInfo] = useState('');
+    const [checkin, setCheckin] = useState('');
+    const [checkout, setCheckout] = useState('');
+    const [maxGuests, setMaxGuests] = useState('');
+
+    function inputHeader(text) {
+        return (
+            <h2 className="text-2xl mt-4">{text}</h2>
+        )
+    }
+
+    function inputDescription(text) {
+        return (
+            <p className="text-sm text-gray-500"> {text}</p>
+        )
+    }
+
+    function preInput(header, description) {
+        return (
+            <>
+                {inputHeader(header)}
+                {inputDescription(description)}
+            </>
+        )
+    }
+
+    async function addPhotoByLink(e) {
+        e.preventDefault();
+        const {data: filename} = await axios.post('/upload-by-link', {link: photoLink});
+        setAddedPhotos(prev => {
+            return [...prev, filename];
+        })
+        setPhotoLink('');
+    }
+
     return (
         <div>
             {/*not click add new, shows the add new places button*/}
@@ -23,18 +66,84 @@ export default function PlacesPage() {
             {action === 'new' && (
                 <div>
                     <form>
-                        <h2 className="text-2xl mt-4">Title</h2>
-                        <p className="text-sm text-gray-500">Title for your place, should be short and catchy for
-                            advertising.</p>
-                        <input type="text" placeholder="title, for example: my lovely aprtment "/>
-                        <h2 className="text-2xl mt-4">Address</h2>
-                        <p className="text-sm text-gray-500">Address to this place.</p>
-                        <input type="text" placeholder="address"/>
-                        <h2 className="text-2xl mt-4">Photos</h2>
-                        <p className="text-sm text-gray-500">More = Better</p>
-                        <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-                            <button className="border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">+</button>
+                        {preInput('Title', 'Title for your place, should be short and catchy for advertising')}
+                        <input type="text"
+                               value={title} onChange={e => setTitle(e.target.value)}
+                               placeholder="title, for example: my lovely aprtment "/>
+
+                        {preInput('Address', 'Address to this place')}
+                        <input type="text"
+                               value={address} onChange={e => setAddress(e.target.value)}
+                               placeholder="address"/>
+
+                        {preInput('Photos', 'more = better')}
+                        <div className="flex gap-2">
+                            <input type="text"
+                                   value={photoLink}
+                                   onChange={e => setPhotoLink(e.target.value)}
+                                   placeholder={'Add using a link...jpg'}/>
+                            <button
+                                onClick={addPhotoByLink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;Photo
+                            </button>
                         </div>
+                        <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+                            {addedPhotos.length > 0 && addedPhotos.map(link => (
+                                <div>
+                                    <img className="rounded-2xl" src={'http://localhost:4000/uploads/' + link} alt="place photo"/>
+                                </div>
+                            ))}
+                            <button
+                                className="flex items-center gap-1 justify-center border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                                    <path strokeLinecap="round" strokeLinejoin="round"
+                                          d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"/>
+                                </svg>
+                                Upload
+                            </button>
+                        </div>
+
+                        {preInput('Description', 'description of the place')}
+                        <textarea
+                            value={description}
+                            onChange={e => setDescription(e.target.value)}
+                        />
+
+                        {preInput('Perks', 'select all the perks of your place')}
+                        <div className="grid gap-2 mt-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6 ">
+                            <Perks selected={perks} onChange={setPerks}/>
+                        </div>
+
+                        {preInput('Extra info', 'house rules, etc')}
+                        <textarea
+                            value={extraInfo}
+                            onChange={e => setExtraInfo(e.target.value)}
+                        />
+                        {preInput('Check in&out times', 'add check in and out, and guests')}
+                        <div className="grid gap-2 sm:grid-cols-3">
+                            <div>
+                                <h3 className="mt-2 -mb-1">Check in time</h3>
+                                <input type="text"
+                                       value={checkin}
+                                       onChange={e => setCheckin(e.target.value)}
+                                       placeholder="14"/>
+                            </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1">Check out time</h3>
+                                <input type="text"
+                                       value={checkout}
+                                       onChange={e => setCheckout(e.target.value)}
+                                       placeholder="11"/>
+                            </div>
+                            <div>
+                                <h3 className="mt-2 -mb-1">Max number of guests</h3>
+                                <input type="number"
+                                       value={maxGuests}
+                                       onChange={e => setMaxGuests(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <button className="primary my-4">Save</button>
                     </form>
                 </div>
             )}
