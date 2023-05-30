1b4c3e404a379ab36ea1b89822849761956a663a
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-05-30 12:23:25 +1000

    feat: update and save user places

--- api/index.js
+++ api/index.js
@@ -127,7 +127,7 @@ app.post('/upload', photosMiddleware.array('photos', 100), async (req, res) => {
 //add new place
 app.post('/places', (req, res) => {
     const {token} = req.cookies;
-    const {title, address, addedPhotos,
+    const {title, address, photos: addedPhotos,
         description, perks, extraInfo,
         checkout, checkin, maxGuests
     } = req.body;
@@ -135,12 +135,48 @@ app.post('/places', (req, res) => {
         if (err) throw err;
       const placeDoc =  await Place.create({
             owner: userData.id,
-          title, address, addedPhotos,
+          title, address, photos: addedPhotos,
           description, perks, extraInfo,
           checkout, checkin, maxGuests,
         });
       res.json(placeDoc);
     });
+})
+
+app.get('/places', (req, res)=>{
+    const {token} = req.cookies;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        const {id} = userData;
+        res.json(await Place.find({owner:id}))
+    })
+})
+
+//redirect to the places we saved
+app.get('/places/:id', async(req, res)=>{
+const{id} = req.params;
+res.json(await Place.findById(id))
+})
+
+//update the saved place
+app.put('/places', async (req, res)=>{
+    const {token} = req.cookies;
+    const {id, title, address, addedPhotos,
+        description, perks, extraInfo,
+        checkout, checkin, maxGuests
+    } = req.body;
+
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        const placeDoc = await Place.findById(id);
+       if(userData.id === placeDoc.owner.toString()) {
+           placeDoc.set({
+               title, address, photos: addedPhotos,
+               description, perks, extraInfo,
+               checkout, checkin, maxGuests
+           });
+           placeDoc.save();
+           res.json('ok');
+       }
+    })
 })
 
 app.listen(4000);
--- api/models/Place.js
+++ api/models/Place.js
@@ -4,7 +4,7 @@ const placeSchema = new mongoose.Schema({
     owner: {type:mongoose.Schema.Types.ObjectId, ref: 'User'},
     title: String,
     address: String,
-    Photos: [String],
+    photos: [String],
     description: String,
     perks: [String],
     extraInfo: String,
