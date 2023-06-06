0a8b4bf393ba56506ece0caac28852c1502e3de1
Sylvia Xie <100256628+SYLVIAXIE1997@users.noreply.github.com>
2023-06-06 13:46:22 +1000

    fix: fix deploy cors err

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ import BookingsPage from "./pages/BookingsPage.jsx";
 import BookingPage from "./pages/BookingPage.jsx";
 
 
-axios.defaults.baseURL = 'http://127.0.0.1:4000';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App(){
